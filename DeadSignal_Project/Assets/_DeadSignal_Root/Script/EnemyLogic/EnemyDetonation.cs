1a4915b50e54f7675536240bc2c4b6edd79c3d65
LiebPR <jplatero773@gmail.com>
2026-03-08 21:02:45 +0100

    DetonateEnemy

---
+++ DeadSignal_Project/Assets/_DeadSignal_Root/Script/EnemyLogic/EnemyDetonation.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using UnityEngine;
+
+public class EnemyDetonation : MonoBehaviour
+{
+    [Header("Explosion")]
+    [SerializeField] float damage = 40f;
+
+    [Header("Timing")]
+    [SerializeField] float expandDuration = 2f;
+    [SerializeField] float maxSizeDuration = 1f;
+
+    [Header("Warning Area")]
+    [SerializeField] SpriteRenderer warningRenderer;
+
+    [Header("Explosion Collider")]
+    [SerializeField] Collider2D explosionTrigger;
+
+    EnemyFSM FSM;
+
+    Vector3 maxScale = new Vector3(8f, 8f, 1f);
+
+    void Awake()
+    {
+        FSM = GetComponent<EnemyFSM>();
+
+        if (warningRenderer != null)
+        {
+            warningRenderer.enabled = false;
+            warningRenderer.transform.localScale = Vector3.zero;
+        }
+
+        if (explosionTrigger != null)
+            explosionTrigger.enabled = false;
+    }
+
+    void OnEnable()
+    {
+        if (FSM != null)
+            FSM.OnStateChanged += OnStateChanged;
+    }
+
+    void OnDisable()
+    {
+        if (FSM != null)
+            FSM.OnStateChanged -= OnStateChanged;
+    }
+
+    void OnStateChanged(EnemyState state)
+    {
+        if (state == EnemyState.Detonate)
+            StartCoroutine(DetonationRoutine());
+    }
+
+    IEnumerator DetonationRoutine()
+    {
+        warningRenderer.enabled = true;
+
+        yield return StartCoroutine(ExpandArea());
+
+        yield return new WaitForSeconds(maxSizeDuration);
+
+        warningRenderer.enabled = false;
+
+        Explode();
+    }
+
+    IEnumerator ExpandArea()
+    {
+        float time = 0f;
+
+        Transform area = warningRenderer.transform;
+        area.localScale = Vector3.zero;
+
+        while (time < expandDuration)
+        {
+            float t = time / expandDuration;
+
+            area.localScale = Vector3.Lerp(Vector3.zero, maxScale, t);
+
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        area.localScale = maxScale;
+    }
+
+    void Explode()
+    {
+        explosionTrigger.enabled = true;
+        StartCoroutine(DisableExplosion());
+    }
+
+    IEnumerator DisableExplosion()
+    {
+        yield return new WaitForSeconds(0.05f);
+
+        explosionTrigger.enabled = false;
+
+        gameObject.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        HealthSystem health = other.GetComponent<HealthSystem>();
+
+        if (health != null)
+            health.TakeDamage(damage);
+    }
+}
